ab80d63a65c2709a4e31fbaaf6b03173f7376775
Vadim4699 <strochuk.vadim@gmail.com>
2020-07-08 22:45:48 +0300

    added INT0

--- GccApplication1/main.cpp
+++ GccApplication1/main.cpp
@@ -9,10 +9,16 @@
 #define F_CPU 1000000U
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 
-
+ISR(INT0_vect)
+{
+	PORTD |= (1 << 6);
+	_delay_ms(700);
+	PORTD &= ~(1 << 6);
+}
 
 int main(void)
 {
@@ -25,24 +31,23 @@ int main(void)
 	DDRC &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3));
 	PORTC &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3));
 	
+	DDRD &= ~(1 << 2);
+	PORTD &= ~(1 << 2);
+	MCUCR = (1 << ISC01)|(1 << ISC00)|(1 << ISC11)|(1 << ISC10);
+	GICR = (1 << INT0);
+	
+	sei();
 	
     while (1) 
     {
-		if(!(PINC & (1 << 1)))
-		{
-			PORTD |= (1 << 6);
-		}
-		if(PINC & (1 << 2))
-		{
-			PORTD |= (1 << 7);
-		}
+		
 		
 		PORTD |= (1 << 5);
 		_delay_ms(500);
 		PORTD &=~ (1 << 5);
 		_delay_ms(500);
 		
-		PORTD &= ~((1 << 6)|(1 << 7));
+		
     }
 }
 
